408398c0c1f83dbb87f1f5d3e462d505622d2488
John Hodnik <29070834+jrhodnik@users.noreply.github.com>
2020-12-15 15:58:31 -0500

    Clarify some stuff.

--- NpgSqlTests/Program.cs
+++ NpgSqlTests/Program.cs
@@ -12,20 +12,28 @@ namespace NpgSqlTests
         {
             using (var db = new DatabaseContext())
             {
+                //Add a new empty record to the database.
                 var id = await addFirst();
 
-                var first = await db.TestModels.SingleAsync(x => x.Id == id);
-                first.PropFour.Add(1234);
+                //Fetch the record
+                var thing = await db.TestModels.SingleAsync(x => x.Id == id);
+                
+                //Add a new string to the List
+                thing.PropFour.Add(1234);
+
+                Console.WriteLine($"The one we modified: {thing.PropFour.First()}. These shouldn't return anything but do: {thing.PropTwo.First()} {thing.PropThree.First()}");
+
+                Console.WriteLine($"Lists of different type are unaffected: {thing.PropOne.FirstOrDefault()}");
             }
         }
 
         static async Task<Guid> addFirst()
         {
             using var db = new DatabaseContext();
-            var newModel = new TestModel();
-            db.TestModels.Add(newModel);
+            var newRec = new TestModel();
+            db.TestModels.Add(newRec);
             await db.SaveChangesAsync();
-            return newModel.Id;
+            return newRec.Id;
         }
     }
 }
